c94955355128b99ad39d1e71c58a3044986118ac
triest <you@example.com>
2019-08-02 20:48:06 +0300

    Add crud for books and authork

---
+++ modules/admin/views/authors/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Authors */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="authors-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'family')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'year_of_birth')->textInput() ?>
+
+    <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ modules/admin/views/books/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Books */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="books-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'year')->textInput() ?>
+
+    <?= $form->field($model, 'id_autor')->textInput() ?>
+
+    <?= $form->field($model, 'id_publishing')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
